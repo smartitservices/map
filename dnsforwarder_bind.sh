69eb1025b623b16bb865943d2b589a7e5789865d
Stefan Beckmann <stefan@beckmann.ch>
2022-03-09 20:06:04 +0100

    Noninteractive

--- dnsforwarder_bind.sh
+++ dnsforwarder_bind.sh
@@ -1,6 +1,7 @@
-apt-get update -y 
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+apt-get update -y
 apt-get upgrade -y
-apt-get install -y bind9 bind9utils bind9-doc
+apt-get install bind9 bind9utils bind9-doc -y
 
 touch /etc/bind/named.conf.options
 cat << EOF > /etc/bind/named.conf.options
@@ -21,4 +22,4 @@ options {
 };
 EOF
 
-sudo bind9 -t && sudo service bind9 restart
+sudo named -t && sudo service named restart
